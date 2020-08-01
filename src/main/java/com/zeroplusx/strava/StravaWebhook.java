9525e8fe6b4e9149cbfbdfb7991b4e3788c57b6f
Willian Paixao <willian@ufpa.br>
2020-08-01 03:25:18 +0200

    Update StravaWebhook class

--- src/main/java/com/zeroplusx/strava/StravaWebhook.java
+++ src/main/java/com/zeroplusx/strava/StravaWebhook.java
@@ -3,6 +3,8 @@ package com.zeroplusx.strava;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import java.io.BufferedWriter;
 import java.net.HttpURLConnection;
@@ -11,6 +13,7 @@ import java.util.logging.Logger;
 public class StravaWebhook implements HttpFunction {
   private static final Logger logger = Logger.getLogger(StravaWebhook.class.getName());
   private static final String token = System.getProperty("VERIFY_TOKEN");
+  private static final Gson gson = new Gson();
 
   @Override
   public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
@@ -26,8 +29,11 @@ public class StravaWebhook implements HttpFunction {
         httpResponse.setStatusCode(HttpURLConnection.HTTP_OK);
         break;
       case "POST":
-        httpResponse.setStatusCode(HttpURLConnection.HTTP_OK);
-        writer.write("EVENT_RECEIVED");
+        JsonObject body = gson.fromJson(httpRequest.getReader(), JsonObject.class);
+        if (body.has("object_type")) {
+          httpResponse.setStatusCode(HttpURLConnection.HTTP_OK);
+          writer.write("EVENT_RECEIVED");
+        }
         break;
       default:
         httpResponse.setStatusCode(HttpURLConnection.HTTP_BAD_METHOD);
--- src/test/java/com/zeroplusx/strava/StravaWebhookTest.java
+++ src/test/java/com/zeroplusx/strava/StravaWebhookTest.java
@@ -1,10 +1,8 @@
 package com.zeroplusx.strava;
 
-import java.io.BufferedWriter;
-import java.io.StringWriter;
-
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+import com.google.gson.Gson;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,12 +10,23 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.when;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.util.Map;
+
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class StravaWebhookTest {
-  @Mock private HttpRequest httpRequest;
-  @Mock private HttpResponse httpResponse;
+  private static Gson gson = new Gson();
+
+  @Mock
+  private HttpRequest httpRequest;
+  @Mock
+  private HttpResponse httpResponse;
 
   private BufferedWriter writerOut;
   private StringWriter responseOut;
@@ -32,10 +41,16 @@ public class StravaWebhookTest {
 
   @Test
   public void StravaWebhookPostTest() throws Exception {
+    String requestJson = gson.toJson(Map.of("object_type", "activity"));
+    BufferedReader bodyReader = new BufferedReader(new StringReader(requestJson));
+
     when(httpRequest.getMethod()).thenReturn("POST");
+    when(httpRequest.getReader()).thenReturn(bodyReader);
+
     new StravaWebhook().service(httpRequest, httpResponse);
 
     writerOut.flush();
+    verify(httpResponse, times(1)).setStatusCode(HttpURLConnection.HTTP_OK);
     Assert.assertEquals("EVENT_RECEIVED", responseOut.toString());
   }
 }
